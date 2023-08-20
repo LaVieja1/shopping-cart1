868cc37c553ac0a038f44c159ad4345da8f15af6
santicano <canosantiago404@gmail.com>
2023-08-20 20:54:31 -0300

    contenido header prueba

---
+++ src/components/headerNav/HeaderNav.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
+import PersonIcon from '@mui/icons-material/Person';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import IconButton from '@mui/material/IconButton';
+import Badge from '@mui/material/Badge';
+
+export const HeaderNav = () => {
+    return (
+        <nav className="headerNav">
+            <div className="message"><p>WE SHIP WORLDWIDE</p></div>
+            <div className="links">
+                <Link to="/">
+                    <Badge>
+                        <IconButton>
+                            <ShoppingCartIcon />
+                        </IconButton>
+                    </Badge>
+                </Link>
+                <PersonIcon />
+            </div>
+        </nav>
+    );
+};
