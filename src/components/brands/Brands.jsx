e07a7d304932c1c05953d6f8f05afe5020cf7df9
santicano <canosantiago404@gmail.com>
2023-08-23 20:38:28 -0300

    Añadidos demas brands, todo cambiar a botones

--- src/components/brands/Brands.jsx
+++ src/components/brands/Brands.jsx
@@ -7,6 +7,18 @@ export const Brands = () => {
                 <img src='/src/assets/img/ImagenlandingPage.png' alt='jordan' className='jordan-img'></img>
                 <div className="jordan">JORDAN</div>
             </div>
+            <h2 className='brands'>BRANDS</h2>
+            <div className='brands-container'>
+                <div className='nike-container'>
+                    <div className='nike button'>NIKE</div>
+                </div>
+                <div className='adidas-container'>
+                    <div className='adidas button'>ADIDAS</div>
+                </div>
+                <div className='puma-container'>
+                    <div className='puma button'>PUMA</div>
+                </div>
+            </div>
         </main>
     )
 }
