6b2ed4cd29ff035a84535c5b516d25d559a3e993
santicano <canosantiago404@gmail.com>
2023-08-23 16:19:01 -0300

    Añadido jordan

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 import {HeaderNav} from './components/headerNav/HeaderNav';
 import { BrowserRouter } from 'react-router-dom';
 import { Nav } from './components/nav/Nav';
+import { Brands } from './components/brands/Brands';
 
 function App() {
   return (
     <BrowserRouter>
       <HeaderNav />
       <Nav />
+      <Brands />
     </BrowserRouter>
   )
 }
---
+++ src/components/brands/Brands.jsx
@@ -0,0 +1,12 @@
+import './Brands.css';
+
+export const Brands = () => {
+    return (
+        <main>
+            <div className="jordan-container">
+                <img src='/src/assets/img/ImagenlandingPage.png' alt='jordan' className='jordan-img'></img>
+                <div className="jordan">JORDAN</div>
+            </div>
+        </main>
+    )
+}
